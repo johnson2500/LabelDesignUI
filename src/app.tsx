052e3d5bad513459f42ffbcf883d9c620341a73b
ryan <johnson2500@live.com>
2026-02-25 20:14:48 -0500

    working logs

--- src/app.tsx
+++ src/app.tsx
@@ -23,22 +23,22 @@ export const App = () => {
     }
   };
 
-  // if (!authenticated) {
-  //   return (
-  //     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "4em" }}>
-  //       <form onSubmit={handleSubmit}>
-  //         <input
-  //           type="password"
-  //           placeholder="Enter password"
-  //           value={password}
-  //           onInput={(e: any) => setPassword(e.target.value)}
-  //           style={{ marginRight: "0.5em" }}
-  //         />
-  //         <button type="submit">Submit</button>
-  //       </form>
-  //     </div>
-  //   );
-  // }
+  if (!authenticated) {
+    return (
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "4em" }}>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onInput={(e: any) => setPassword(e.target.value)}
+            style={{ marginRight: "0.5em" }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
