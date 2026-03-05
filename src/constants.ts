539159c7035089653f53925f65c1eb9cb46e58cb
ryan <johnson2500@live.com>
2026-03-04 21:31:25 -0500

    cool

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const baseUrl = "http://localhost:3000"; //"https://label-desgin-api-a0acdb86ab47.herokuapp.com";
+export const baseUrl = "https://label-desgin-api-a0acdb86ab47.herokuapp.com";
